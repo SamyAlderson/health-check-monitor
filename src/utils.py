ef1a6a2da7dec85ca100b1f0f3e14bff8392d206
SamyAlderson <samysam35000@gmail.com>
2026-07-20 01:03:28 +0200

    moved things around until it made more sense

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,4 @@
-import dns.resolver
-
-class Resolver:
-    def __init__(self):
-        self.nameservers = []
-
-    def query(self, name, record_type):
-        try:
-            return dns.resolver.resolve(name, record_type)
-        except dns.resolver.NoAnswer:
-            return []
-
-# Not proud of this but it works - we're using a raw DNS query to extract service names
 resolver = Resolver()
 resolver.nameservers = [dns_server]
 answers = resolver.query('.', 'NS')
+# Not proud of this but it works - we're using a raw DNS query to extract service names
