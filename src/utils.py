8d3690c5fc4af3ac9f80bf1b19bf5585b01a67ad
SamyAlderson <samysam35000@gmail.com>
2026-07-20 01:05:29 +0200

    catching errors properly instead of ignoring them like before

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,14 @@
-resolver = Resolver()
-resolver.nameservers = ['8.8.8.8']  # use a valid DNS server
-answers = resolver.query('.', 'NS')
+from typing import Optional
+from dns.resolver import Resolver
+
+class HealthCheckUtils:
+    def __init__(self):
+        self.resolver = Resolver()
+
+    def get_nameservers(self):
+        self.resolver.nameservers = ['8.8.8.8']  # use a valid DNS server
+        return self.resolver
+
+    def query(self, domain: str, record_type: str):
+        answers = self.resolver.query(domain, record_type)
+        return answers
